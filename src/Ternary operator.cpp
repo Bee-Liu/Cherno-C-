cdc489482b63cfafc4e96ebea0b6ccf89b3111a9
Bee-Liu <2890263330@qq.com>
2022-12-18 21:51:48 +0800

    Three count

--- src/Ternary operator.cpp
+++ src/Ternary operator.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
-int main()
-{
-    
-    return 0;
-}
+// static int s_Level = 20;
+// static int s_Speed = 3;
+// int main()
+// {
+//     //FIXME:这块代码和下面的一行代码实现的效果相同
+//     /* if(s_Level > 5)
+//         s_Speed = 10;
+//     else
+//         s_Speed = 5; */
+
+//     s_Speed = s_Level > 5 ? 12:6;
+//     std::cout << "s_Speed: " << s_Speed << std::endl;
+
+//     std::string rank=s_Level>10 ? "Master":"Beginner";
+//     std::cout << "rank: " << rank << std::endl;
+
+//     s_Speed = s_Level>5 ?s_Level>10? 15:10:5;
+//     std::cout << "s_Speed: " << s_Speed << std::endl;
+//     return 0;
+// }
