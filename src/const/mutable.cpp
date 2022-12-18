d23acb44813188cc229acc3237d4d01ae3dae2e6
Bee-Liu <2890263330@qq.com>
2022-12-18 12:20:40 +0800

    mutable

--- src/const/mutable.cpp
+++ src/const/mutable.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
 
+class Entity
+{
+    private:
+    std::string name;
+    public:
+    const std::string& getName() const { return name; }
+};
+
 int main()
 {
+
     using namespace std::string_literals;
     std::string name = "Hello"s+"World";
     std::cout << name << std::endl;
