ecf4ee06512d19ee8402fc3cf337bdbfab4591f3
Bee-Liu <2890263330@qq.com>
2022-12-19 19:40:22 +0800

    ..

--- src/new/new.cpp
+++ src/new/new.cpp
@@ -1,31 +1,32 @@
-// #include <iostream>
-// #include <string>
+#include <iostream>
+#include <string>
 
-// using String = std::string;
+using String = std::string;
 
-// class Entity
-// {
-// private:
-//     String m_name;
-// public:
-//     Entity(): m_name("Unknown") {}
-//     Entity(const String& name): m_name(name) {}
+class Entity
+{
+private:
+    String m_name;
+public:
+    Entity(): m_name("Unknown") {}
+    Entity(const String& name): m_name(name) {}
 
-//     const String& Getname() const { return m_name; }
-// };
+    const String& Getname() const { return m_name; }
+};
 
-// int main()
-// {
-//     int a = 2;//FIXME:4 bytes
-//     int* b=new int[50]; //FIXME:200 bytes
+int main()
+{
     
-//     //FIXME:placement new
-//     Entity* e = new Entity; //FIXME:他调用了Entity构造函数  FIXME:Entity* e = new(b) Entity;将b分配的内存给e；
-//     Entity* e1 = (Entity*)malloc(sizeof(Entity));//FIXME:仅仅分配内存，然后给我们一个指向那个内存的地址，并没有调用构造函数
+    int a = 2;//FIXME:4 bytes
+    int* b=new int[50]; //FIXME:200 bytes
     
-//     free(e1);
-//     delete[] b;
-//     delete e;
+    //FIXME:placement new
+    Entity* e = new Entity; //FIXME:他调用了Entity构造函数  FIXME:Entity* e = new(b) Entity;将b分配的内存给e；
+    Entity* e1 = (Entity*)malloc(sizeof(Entity));//FIXME:仅仅分配内存，然后给我们一个指向那个内存的地址，并没有调用构造函数
     
-//     return 0;
-// }
+    free(e1);
+    delete[] b;
+    delete e;
+    
+    return 0;
+}
